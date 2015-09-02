6b5aa08d5e01eb269e78c9c7724fff0d254dd71d
Phil Oats <phillip.oats@gmail.com>
2015-09-02 12:17:13 +0100

    Create script

---
+++ Assets/Scripts/ObjectScripts/Lightning.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightning : MonoBehaviour {
+
+	float amountOfOff = 0.9f;
+	float amountOfOn = 0.015f; 
+	Light l;
+
+	void Start(){
+		l = GetComponent<Light> ();
+	}
+
+	void Update()
+	{
+		if(l.enabled && (Random.value > amountOfOff))
+			l.enabled = false;
+		else
+			if(Random.value < amountOfOn)
+				l.enabled = true;
+	}
+	
+
+}
