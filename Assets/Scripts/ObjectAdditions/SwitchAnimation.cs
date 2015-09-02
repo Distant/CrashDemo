aa0339ba1b2d7803d80bdf2f410bde3cc84bf39f
Phil Oats <phillip.oats@gmail.com>
2015-09-02 12:16:10 +0100

    Create script
    Replaces individual scripts for switching between a pair of on/off
    animations

---
+++ Assets/Scripts/ObjectAdditions/SwitchAnimation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchAnimation : MonoBehaviour {
+
+	private Animator anim;
+	private bool active;
+	[SerializeField] private float activeTime;
+	[SerializeField] private float inactiveTime;
+	[SerializeField] private float animOffset;
+
+	private float animTime;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		animTime = anim.runtimeAnimatorController.animationClips [0].length;
+		StartCoroutine (Animate ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public IEnumerator Animate(){
+		yield return new WaitForSeconds (animOffset + animTime);
+		while (true) { 
+			if (active) {
+				anim.SetBool ("active", active);
+				active = false;
+				yield return new WaitForSeconds (activeTime);
+			} else {
+				anim.SetBool ("active", active);
+				active = true;
+				yield return new WaitForSeconds (inactiveTime);
+			}
+		}
+	}
+}
