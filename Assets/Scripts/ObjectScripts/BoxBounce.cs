4cbe0ce8257a574f6414226e60bd9043a7e872cb
Phil Oats <phillip.oats@gmail.com>
2015-09-06 18:16:28 +0100

    Remove functions not overridden

--- Assets/Scripts/ObjectScripts/BoxBounce.cs
+++ Assets/Scripts/ObjectScripts/BoxBounce.cs
@@ -5,12 +5,6 @@ public class BoxBounce : Box  {
 
     // Use this for initialization
     public override void Start () {
-        base.Start();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-   
+		base.Start ();
+	}   
 }
--- Assets/Scripts/ObjectScripts/BoxInvisible.cs
+++ Assets/Scripts/ObjectScripts/BoxInvisible.cs
@@ -9,12 +9,6 @@ public class BoxInvisible : Box
 	{
         base.Start();
     } 
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
 
 	public override void HitPlayer(GameObject g, Vector3 point) {
         Enable();
