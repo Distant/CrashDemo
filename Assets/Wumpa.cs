239ad5cfdcd752718cdce1f2d50d00b0c7d3c40c
Phil Oats <phillip.oats@gmail.com>
2015-09-06 18:18:02 +0100

    Create script

---
+++ Assets/Wumpa.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wumpa : MonoBehaviour {
+
+	private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+		levelManager = GameObject.FindGameObjectWithTag ("LevelManager").GetComponent < LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(Camera.main.transform.position, Vector3.up);
+	}
+
+
+	public void OnTriggerEnter(Collider col){
+		if (col.tag == "Player") {
+			levelManager.InventoryManager.AddWumpa (transform);
+			Destroy(this.gameObject);
+		}
+	}
+}
