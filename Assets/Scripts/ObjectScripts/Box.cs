d72136b8292ad52a72b2b65b1c88c174341727a1
Phil Oats <phillip.oats@gmail.com>
2015-09-06 18:15:46 +0100

    Separate functionality and make more flexible
    Easier to use as a base class and is more flexible with the level system

--- Assets/Scripts/ObjectScripts/Box.cs
+++ Assets/Scripts/ObjectScripts/Box.cs
@@ -1,29 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Box : MonoBehaviour 
+public class Box : MonoBehaviour
 {
 	protected LevelManager levelManager;
 	protected CharacterControl player;
 	protected SoundManager soundManager;
-
 	[SerializeField] private bool breakable = true;
 	[SerializeField] private bool canSpin = true;
 	[SerializeField] private bool canBounce = true;
 	[SerializeField] private int bounceTotal = 0;
 	[SerializeField] private float jumpSpeed = 6f;
 	[SerializeField] protected float coolDownTime;
-
-	public BoxCollider collider = null;
+	[SerializeField] private int wumpaCount = 5;
+	[SerializeField] private bool autoWumpas;
+	public BoxCollider BoxCollider = null;
 	protected Animator animator;
 	private ParticleSystem particles;
 	protected float cooldown;
 	protected bool onCooldown = false;
+	public Transform wumpaObject;
 
 	// Use this for initialization
 	public virtual void Start ()
 	{	
 		animator = GetComponent<Animator> ();
+		if (animator != null)
+			animator.logWarnings = false;
 		particles = GetComponent<ParticleSystem> ();
 		levelManager = GameObject.FindGameObjectWithTag ("LevelManager").GetComponent<LevelManager> ();
 		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<CharacterControl> ();
@@ -46,52 +49,77 @@ public class Box : MonoBehaviour
 
 	public virtual void HitPlayer (GameObject g, Vector3 point)
 	{
-		//if (g.transform.position.y > transform.position.y + 0.5f && (player.Velocity.y < -2f || player.Jumping)) {
-
-		// hit from below
-		if (player.ControllerBounds.max.y <= transform.position.y - 0.24f && player.Jumping && player.Velocity.y > 0) {
+		if (PlayerBelow (g, point)) {
 			if (player.Spinning) {
 				OnSpin ();
 			} else {
 				OnBounce (false);
 			}
-			// hit from above
-		} else if (((player.ControllerBounds.min.y + 0.1f >= transform.position.y + 0.24f) && ((player.Velocity.y < -2f && !player.Jumping) || (player.Velocity.y < 0f && player.Jumping)))) {
+
+		} else if (PlayerAbove (g, point)) {
 			if (player.Spinning) {
-				player.Stop () ;
+				player.Stop ();
 				OnSpin ();
-			}
-			if (canBounce)
+			} else if (canBounce) {
 				OnBounce (true);
+			}
 		}
+	}
+
+	private bool PlayerAbove (GameObject g, Vector3 point)
+	{
+		return (player.ControllerBounds.min.y + 0.1f >= transform.position.y + 0.24f) && ((player.Velocity.y < -2f && !player.Jumping) || (player.Velocity.y < 0f && player.Jumping));
+	}
+
+	private bool PlayerBelow (GameObject g, Vector3 point)
+	{
+		return player.ControllerBounds.max.y <= transform.position.y - 0.24f && player.Jumping && player.Velocity.y > 0;
 	}
 
 	public virtual void OnSpin ()
 	{
 		Break ();
 	}
+
+	public virtual void Trigger ()
+	{
+		if (!onCooldown) {
+			onCooldown = true;
+			if (animator != null)
+				animator.SetBool ("on_cooldown", onCooldown);
+			soundManager.PlayClipAtPoint ("bouncy_box_bounce", transform.position, 0.05f);
+			cooldown = coolDownTime;
+		}
+	}
     
+	public virtual void BouncePlayer (bool above)
+	{
+		player.Stop ();
+		if (above) {
+			player.Jump (jumpSpeed);
+		}
+		if (bounceTotal > 0) {
+			if (animator != null)
+				animator.SetTrigger (above ? "bounce" : "bounce_below");
+			SingleBounce ();
+		} else
+			Break ();
+	}
+
 	public virtual void OnBounce (bool above)
 	{
-		 if (!onCooldown) {
-			onCooldown = true;
-			if (animator != null) animator.SetBool ("on_cooldown", onCooldown);
-			soundManager.PlayClipAtPoint ("bouncy_box_bounce", transform.position, 0.07f);
-			cooldown = coolDownTime;
-			player.Stop ();
-			if (above)
-				player.Jump (jumpSpeed);
-			if (bounceTotal > 0) {
-				if (animator != null) animator.SetTrigger (above ? "bounce" : "bounce_below");
-				bounceTotal --;
-				//soundManager.PlayClipAtPoint ("wumpa_collect", transform.position, 0.03f);
-			} else
-				Break ();
-		}
+		Trigger ();
+		BouncePlayer (above);
+	}
+
+	protected virtual void SingleBounce ()
+	{
+		levelManager.InventoryManager.AddWumpa (transform);
+		bounceTotal --;
 	}
 	 
 	public void OnTriggerEnter (Collider col)
-	{
+	{ 
 		if (col.tag == "player_big_collider") {
 			if (player.Spinning && canSpin) {
 				OnSpin ();
@@ -105,27 +133,34 @@ public class Box : MonoBehaviour
 	{
 		if (col.tag == "player_big_collider") {
 			CharacterControl player = col.GetComponentInParent<CharacterControl> ();
-			player.NotTouching (this);
+			player.NotTouching (this); 
 		}
 	}
 
 	public void Break ()
 	{
 		if (breakable) {
-			soundManager.PlayClipAtPoint ("box_break", transform.position, 0.05f);
-			levelManager.InventoryManager.BreakBox (5);
+			soundManager.PlayClipAtPoint ("box_break", transform.position, 0.08f);
 			player.NotTouching (this);
-			//soundManager.PlayClipAtPoint ("wumpa_collect", transform.position, 0.03f);
-			StartCoroutine(DestroyObject());
+			if (autoWumpas)
+				levelManager.InventoryManager.AddWumpas (wumpaCount, transform); 
+			else {
+				for (int i = 0; i < wumpaCount; i++) {
+					Instantiate (wumpaObject).position = wumpaCount == 1 ? transform.position : transform.position +  new Vector3 (Random.Range (-0.3f, 0.3f), Random.Range (-0.075f, 0.075f), Random.Range(-0.1f, 0.1f));
+				}
+			}
+			//levelManager.InventoryManager.AddWumpas (wumpaCount,Camera.main.WorldToScreenPoint(transform.position));
+			StartCoroutine (DestroyObject ());
 		}
 	}
 
-	public IEnumerator DestroyObject(){
+	public IEnumerator DestroyObject ()
+	{
 		foreach (BoxCollider col in GetComponents<BoxCollider> ()) {
 			col.enabled = false;
 		}
 		GetComponentInChildren<MeshRenderer> ().enabled = false;
-		particles.Play();
+		particles.Play ();
 		yield return new WaitForSeconds (2f);
 		Destroy (gameObject);
 	}
